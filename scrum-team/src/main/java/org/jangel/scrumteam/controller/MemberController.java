e0af5956acf3584dfef2c107046fe2bc4b4a7bcf
jangel <juan.angel.gascon@gmail.com>
2023-04-18 14:29:45 +0200

    Annotation moved

--- scrum-team/src/main/java/org/jangel/scrumteam/controller/MemberController.java
+++ scrum-team/src/main/java/org/jangel/scrumteam/controller/MemberController.java
@@ -7,14 +7,11 @@ import org.jangel.scrumteam.exception.MemberNotFoundException;
 import org.jangel.scrumteam.model.Member;
 import org.jangel.scrumteam.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 @RequestMapping("/v1/member")
@@ -22,29 +19,15 @@ public class MemberController {
 	@Autowired
 	private MemberService memberService;
 
-	@Value("${enable.random.fail}")
-	private boolean randomFail;
-
 	@GetMapping
 	public ResponseEntity<List<Member>> getMembers() {
 		return ResponseEntity.ok(memberService.getMembers());
 	}
 
 	@GetMapping("{memberId}")
-	@CircuitBreaker(name = "memberService")
 	public ResponseEntity<Member> getMember(@PathVariable("memberId") int memberId)
 			throws MemberNotFoundException, TimeoutException {
 		Member member = memberService.getMember(memberId);
-
-		if (randomFail && Math.random() > .5) {
-			try {
-				Thread.sleep(3000);
-				throw new TimeoutException();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
 
 		return ResponseEntity.ok(member);
 	}
--- scrum-team/src/main/java/org/jangel/scrumteam/service/MemberService.java
+++ scrum-team/src/main/java/org/jangel/scrumteam/service/MemberService.java
@@ -10,17 +10,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @Service
 public class MemberService {
 	@Autowired
 	private MemberRepository memberRepository;
 
+	@Value("${enable.random.fail}")
+	private boolean randomFail;
+
 	public List<Member> getMembers() {
 		return memberRepository.findAll();
 	}
 
+	@CircuitBreaker(name = "memberService")
 	public Member getMember(int id) throws MemberNotFoundException, TimeoutException {
 		Member member = null;
+
+		if (randomFail && Math.random() > .0001) {
+			try {
+				Thread.sleep(3000);
+				throw new TimeoutException();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 
 		member = memberRepository.findById(id).orElse(null);
 
